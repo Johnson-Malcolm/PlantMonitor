75d35e88ba0ffbc9901779ccc83839fc3ca20cd9
malcolm <majohnson01@gmail.com>
2019-11-24 14:55:16 -0800

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * FinalProject.c
+ *
+ * Created: 11/24/2019 2:44:23 PM
+ * Author : majoh
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
